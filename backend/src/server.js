8ab620acf011fcc2ac56f389ee6b0ee19a3a3602
mohammadtehreem <tehreemmohammad@gmail.com>
2024-07-15 16:16:32 +0530

    cors fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const auth = require("../middleware/auth");
 const bookRoutes = require("../routes/bookRoutes");
 const cors = require("cors");
 const port = process.env.PORT;
-app.use(cors({ origin: [process.env.FRONTEND_URL] }));
+app.use(cors());
 app.use(express.json());
 app.use("/users", userRoute);
 app.use("/orders", auth, orderRoute);
