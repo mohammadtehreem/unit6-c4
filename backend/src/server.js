eda2c8e3072d736ecbdaf95507a068c57767e4aa
mohammadtehreem <tehreemmohammad@gmail.com>
2024-07-15 16:12:03 +0530

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 require("dotenv").config();
 const db = require("../config/db");
-const sqlConnect = require("../config/sql")
+const sqlConnect = require("../config/sql");
 const app = express();
 const userRoute = require("../routes/userRoute");
 const orderRoute = require("../routes/orderRoute");
 const auth = require("../middleware/auth");
-const bookRoutes = require("../routes/bookRoutes")
+const bookRoutes = require("../routes/bookRoutes");
+const cors = require("cors");
 const port = process.env.PORT;
+app.use(cors({ origin: [process.env.FRONTEND_URL] }));
 app.use(express.json());
 app.use("/users", userRoute);
-app.use("/orders",auth, orderRoute);
+app.use("/orders", auth, orderRoute);
 app.use("/books", auth, bookRoutes);
-app.get("/", (req, res)=>{
-    res.send("This is homepage")
-})
+app.get("/", (req, res) => {
+  res.send("This is homepage");
+});
 
-app.listen(port, async()=>{
-    await db;
-    await sqlConnect;
-    console.log("Server is live on http://localhost:"+port);
-})
+app.listen(port, async () => {
+  await db;
+  await sqlConnect;
+  console.log("Server is live on http://localhost:" + port);
+});
